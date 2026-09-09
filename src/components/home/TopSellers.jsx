675c1ae69fa7793cbb3349b3af34aa40dbb06cb3
caleb <caleb@example.com>
2026-09-08 21:40:45 -0500

    TopSeller2

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -2,26 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
-const TopSellers = () => {
-  const [sellers, setSellers] = useState([]);
-
-  useEffect(() => {
-    const fetchSellers = async () => {
-      try {
-        const response = await fetch(
-          "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
-        );
-        const data = await response.json();
-        console.log("Top Sellers API:", data);
-
-        setSellers(data);
-      } catch (error) {
-        console.error("Error fetching sellers:", error);
-      }
-    };
-
-    fetchSellers();
-  }, []);
 
   return (
     <section id="section-popular" className="pb-5">
