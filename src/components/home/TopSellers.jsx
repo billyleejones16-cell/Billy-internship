d7f5b6fe5f7637795c8f4f1a67182033879aba53
caleb <caleb@example.com>
2026-09-08 21:24:45 -0500

    TopSeller2

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -23,4 +23,47 @@ const TopSellers = () => {
     fetchSellers();
   }, []);
 
-  
+  return (
+    <section id="section-popular" className="pb-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="text-center">
+              <h2>Top Sellers</h2>
+              <div className="small-border bg-color-2"></div>
+            </div>
+          </div>
+          <div className="col-md-12">
+            <ol className="author_list">
+              {sellers.map((seller) => (
+                <li key={seller.id}>
+                  <div className="author_list_pp">
+                    <Link to={`/author/${seller.authorId}`}>
+                      <img
+                        className="lazy pp-author"
+                        src={seller.authorImage || AuthorImage}
+                        alt={seller.authorName}
+                      />
+                      <i className="fa fa-check"></i>
+                    </Link>
+                  </div>
+                  <div className="author_list_info">
+                    <Link to={`/author/${seller.authorId}`}>
+                    {seller.authorName}
+                    </Link>
+                    <span>
+                      {seller.price} ETH
+                    </span>
+
+                  </div>
+                </li>
+              ))}
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TopSellers;
